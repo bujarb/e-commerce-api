14f37be266c38100f08c2c5660aec12df88e6cc9
Bujar Begisholli <begisholli.bujar@gmail.com>
2018-01-31 15:22:11 +0100

    commit

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Exceptions;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+trait ExceptionTrait
+{
+
+  function apiException($request,$e){
+    if ($this->isModel($e)) {
+      return response([
+        'errors'=>'Product Model not found'
+      ],Response::HTTP_NOT_FOUND);
+    }
+
+    if ($this->isHttp($e)) {
+      return response([
+        'errors'=>'Route not found'
+      ],Response::HTTP_NOT_FOUND);
+    }
+
+    return parent::render($request, $exception);
+  }
+
+  public function isModel($e){
+    return $e instanceof ModelNotFoundException;
+  }
+
+  public function isHttp($e){
+    return $e instanceof NotFoundHttpException;
+  }
+}
